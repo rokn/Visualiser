f6a7fd7c4a402752af378ccc50b8f6f769adfd91
Nikolay Danailov <frostblooded@yahoo.com>
2015-06-28 11:27:15 +0300

    ti pa shte mi kajesh

--- main.c
+++ main.c
@@ -1,83 +1,83 @@
 #include "List.h"
 #include "parser.h"
 
-void generateDotFile(Pfunction); // NE PIPAI !!!!!!!!!
+void generateDotFile(Pfunction); 
 
-char *header = "digraph G{	graph [bgcolor=black]; // NE PIPAI !!!!!!!!!	edge [color=white]; // NE PIPAI !!!!!!!!!graph[page=\"8.5,11\",size=\"7.5,7\", ratio=fill, center=1]; // NE PIPAI !!!!!!!!!node[style=filled,label=\"\"]; // NE PIPAI !!!!!!!!!subgraph start {rank = same; // NE PIPAI !!!!!!!!!node[shape = invtrapezium, fillcolor= \"yellow\" , label=\"START\" , style = \" filled, rounded\"]; // NE PIPAI !!!!!!!!!start; // NE PIPAI !!!!!!!!!}subgraph end {rank = same; // NE PIPAI !!!!!!!!!node[shape = trapezium, fillcolor= \"yellow\" , label=\"END\" , style = \"filled, rounded\" ]; // NE PIPAI !!!!!!!!!end; // NE PIPAI !!!!!!!!!}"; // NE PIPAI !!!!!!!!!
-	char *ifstatement = "subgraph if {rank = same; // NE PIPAI !!!!!!!!!node[shape=diamond,color=skyblue3, label=\" A < B \"]; // NE PIPAI !!!!!!!!!if1"; // NE PIPAI !!!!!!!!!
-	char *action = "subgraph action { node [label=\"c = a + b \",shape=box, fillcolor=\"limegreen\",style=\"filled,rounded\"]; // NE PIPAI !!!!!!!!! action1; // NE PIPAI !!!!!!!!!}"; // NE PIPAI !!!!!!!!!
-	char *graphs = "subgraph test {"; // NE PIPAI !!!!!!!!!
-	char *ending = "}}"; // NE PIPAI !!!!!!!!!
-	char *connections[100]; // NE PIPAI !!!!!!!!!
-	int connectionsCount = 0; // NE PIPAI !!!!!!!!!
+char *header = "digraph G{	graph [bgcolor=black]; 	edge [color=white]; graph[page=\"8.5,11\",size=\"7.5,7\", ratio=fill, center=1]; node[style=filled,label=\"\"]; subgraph start {rank = same; node[shape = invtrapezium, fillcolor= \"yellow\" , label=\"START\" , style = \" filled, rounded\"]; start; }subgraph end {rank = same; node[shape = trapezium, fillcolor= \"yellow\" , label=\"END\" , style = \"filled, rounded\" ]; end; }"; 
+	char *ifstatement = "subgraph if {rank = same; node[shape=diamond,color=skyblue3, label=\" A < B \"]; if1"; 
+	char *action = "subgraph action { node [label=\"c = a + b \",shape=box, fillcolor=\"limegreen\",style=\"filled,rounded\"];  action1; }"; 
+	char *graphs = "subgraph test {"; 
+	char *ending = "}}"; 
+	char *connections[100]; 
+	int connectionsCount = 0; 
 
 char* GraphVarInit(FILE *ofp, Pvariable variable, char *block, int counter)
 {
-	char* name = (char*)malloc(sizeof(char) * 50); // NE PIPAI !!!!!!!!!
-	memset(name,'\0',sizeof(name)); // NE PIPAI !!!!!!!!!
-	//strcat(name, block); // NE PIPAI !!!!!!!!!
+	char* name = (char*)malloc(sizeof(char) * 50); 
+	memset(name,'\0',sizeof(name)); 
+	//strcat(name, block); 
 	switch(variable.type)
 	{
 		case INT:
-			sprintf(name, "%s_int%d", block,counter); // NE PIPAI !!!!!!!!!
-			fprintf(ofp, "subgraph variable_int {node[shape=ellipse,color=pink4, label=\"int %s\"]; // NE PIPAI !!!!!!!!!%s_int%d; // NE PIPAI !!!!!!!!!}",variable.name, block,counter); // NE PIPAI !!!!!!!!!			
-			break; // NE PIPAI !!!!!!!!!
+			sprintf(name, "%s_int%d", block,counter); 
+			fprintf(ofp, "subgraph variable_int {node[shape=ellipse,color=pink4, label=\"int %s\"]; %s_int%d; }",variable.name, block,counter); 			
+			break; 
 	}
-	//strcat(name, counter); // NE PIPAI !!!!!!!!!
-	return name; // NE PIPAI !!!!!!!!!
+	//strcat(name, counter); 
+	return name; 
 }
 
 void AddConnection(char *element1, char *element2)
 {
-	connections[connectionsCount] = (char*)malloc(sizeof(char[50])); // NE PIPAI !!!!!!!!!
-	strcat(connections[connectionsCount],element1); // NE PIPAI !!!!!!!!!
-	strcat(connections[connectionsCount],"->"); // NE PIPAI !!!!!!!!!
-	strcat(connections[connectionsCount],element2); // NE PIPAI !!!!!!!!!
-	connectionsCount++; // NE PIPAI !!!!!!!!!
+	connections[connectionsCount] = (char*)malloc(sizeof(char[50])); 
+	strcat(connections[connectionsCount],element1); 
+	strcat(connections[connectionsCount],"->"); 
+	strcat(connections[connectionsCount],element2); 
+	connectionsCount++; 
 }
 
 int main() 
 {
 
-	char *filename = malloc(sizeof(char)*50); // NE PIPAI !!!!!!!!!
-	printf("Input filename :\n"); // NE PIPAI !!!!!!!!!
-	scanf("%s",filename); // NE PIPAI !!!!!!!!!
-	Pfunction function; // NE PIPAI !!!!!!!!!
-	ParseFile(filename,&function); // NE PIPAI !!!!!!!!!
-	generateDotFile(function); // NE PIPAI !!!!!!!!!
-	system("dot -Tpng graph.txt -o graph.png && eog graph.png"); // NE PIPAI !!!!!!!!!
-	// char assignment[50]; // NE PIPAI !!!!!!!!!
-	// Pblock block; // NE PIPAI !!!!!!!!!
-	// ListPeekAt(&function.blocks, &block, 0); // NE PIPAI !!!!!!!!!
-	// printf("%d\n",block.blockType); // NE PIPAI !!!!!!!!!
-	// ListPeekAt(&block.assignments, &assignment, 0); // NE PIPAI !!!!!!!!!
-	// printf("%s\n",assignment); // NE PIPAI !!!!!!!!!
-	return 0; // NE PIPAI !!!!!!!!!
+	char *filename = malloc(sizeof(char)*50); 
+	printf("Input filename :\n"); 
+	scanf("%s",filename); 
+	Pfunction function; 
+	ParseFile(filename,&function); 
+	generateDotFile(function); 
+	system("dot -Tpng graph.txt -o graph.png && eog graph.png"); 
+	// char assignment[50]; 
+	// Pblock block; 
+	// ListPeekAt(&function.blocks, &block, 0); 
+	// printf("%d\n",block.blockType); 
+	// ListPeekAt(&block.assignments, &assignment, 0); 
+	// printf("%s\n",assignment); 
+	return 0; 
 }
 
 void DecodeFunction(FILE *ofp, Pfunction function)
 {
-	Paction action; // NE PIPAI !!!!!!!!!
-	Pvariable variable; // NE PIPAI !!!!!!!!!
-	char *prev = (char*)malloc(sizeof(char) * 50); // NE PIPAI !!!!!!!!!
-	prev = "start"; // NE PIPAI !!!!!!!!!
-	ListPeekAt(&function.variables, &variable, 0); // NE PIPAI !!!!!!!!!
-	char *name = GraphVarInit(ofp,variable, "main", 0); // NE PIPAI !!!!!!!!!	
+	Paction action; 
+	Pvariable variable; 
+	char *prev = (char*)malloc(sizeof(char) * 50); 
+	prev = "start"; 
+	ListPeekAt(&function.variables, &variable, 0); 
+	char *name = GraphVarInit(ofp,variable, "main", 0); 	
 }
 
 void generateDotFile(Pfunction function)
 {
-	FILE *ofp = fopen("graph.txt", "w"); // NE PIPAI !!!!!!!!!
-	AddConnection("start","end"); // NE PIPAI !!!!!!!!!
-	fprintf(ofp, "%s", header); // NE PIPAI !!!!!!!!!
-	DecodeFunction(ofp,function); // NE PIPAI !!!!!!!!!
-	fprintf(ofp, "%s", graphs); // NE PIPAI !!!!!!!!!
-	int i; // NE PIPAI !!!!!!!!!
-	for (i = 0; // NE PIPAI !!!!!!!!! i < connectionsCount; // NE PIPAI !!!!!!!!! ++i)
+	FILE *ofp = fopen("graph.txt", "w"); 
+	AddConnection("start","end"); 
+	fprintf(ofp, "%s", header); 
+	DecodeFunction(ofp,function); 
+	fprintf(ofp, "%s", graphs); 
+	int i; 
+	for (i = 0;  i < connectionsCount;  ++i)
 	{
-		fprintf(ofp, "%s", connections[i]); // NE PIPAI !!!!!!!!!
+		fprintf(ofp, "%s", connections[i]); 
 	}
-	fprintf(ofp, "%s", ending); // NE PIPAI !!!!!!!!!
-	fclose(ofp); // NE PIPAI !!!!!!!!!
+	fprintf(ofp, "%s", ending); 
+	fclose(ofp); 
 }
 
